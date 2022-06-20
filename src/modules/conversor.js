ddab8f073f23f462ed5ae9669ca9e17514d710fc
jaxolv <jaxolv@gmail.com>
2022-06-20 15:27:02 -0300

    instruções no uso das funções presentes em ./src/modules/conversor.js

--- src/modules/conversor.js
+++ src/modules/conversor.js
@@ -1,4 +1,8 @@
 function conversor(tempo) {
+    /* digitar o tempo no formao minutos.segundos;
+    para o usuário, será retornado o valor em minuto.percentual de minuto.
+    ex.: se deseja colocar 1m30s no conversor, digite 1.30 ou 1.3;
+    o valor retornado será 1.5, equivalente a um minuto e meio. */
     minutos = parseInt(tempo)
     segundos = tempo - minutos
     percentualDeMinuto = Math.round(segundos * 100) / 60
@@ -7,6 +11,10 @@ function conversor(tempo) {
 }
 
 function segundos(tempo) {
+    /* digitar o tempo no formao minutos.segundos;
+    para o usuário, será retornado o valor em segundos.
+    ex.: se deseja colocar 1m01s nesta função, digite 1.01;
+    o valor retornado será 61: 1 (60 segundos) + 0.01 (1 segundo). */
     totalSegundos = parseInt(conversor(tempo) * 60)
     return totalSegundos
 }
