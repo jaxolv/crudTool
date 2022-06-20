467f56ed4bcad12ea20c8edae5bd0d24a949e159
Jackson Oliveira <92948351+jaxolv@users.noreply.github.com>
2022-06-20 15:30:17 -0300

    Update conversor.js

--- src/modules/conversor.js
+++ src/modules/conversor.js
@@ -1,5 +1,5 @@
 function conversor(tempo) {
-    /* digitar o tempo no formao minutos.segundos;
+    /* digitar o tempo no formato minutos.segundos;
     para o usuário, será retornado o valor em minuto.percentual de minuto.
     ex.: se deseja colocar 1m30s no conversor, digite 1.30 ou 1.3;
     o valor retornado será 1.5, equivalente a um minuto e meio. */
@@ -11,7 +11,7 @@ function conversor(tempo) {
 }
 
 function segundos(tempo) {
-    /* digitar o tempo no formao minutos.segundos;
+    /* digitar o tempo no formato minutos.segundos;
     para o usuário, será retornado o valor em segundos.
     ex.: se deseja colocar 1m01s nesta função, digite 1.01;
     o valor retornado será 61: 1 (60 segundos) + 0.01 (1 segundo). */
